fc5cc9bf36014630c0d437ba2884316bdb590271
Counter <yuriryabenko436@gmail.com>
2023-07-06 12:06:52 +0300

    chenge header

---
+++ index.js
@@ -0,0 +1,16 @@
+var prevScrollPos = window.scrollY
+var header = document.getElementById('header')
+
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY
+
+  if (prevScrollPos > currentScrollPos) {
+    header.classList.remove('hide-header')
+    header.classList.add('show-header')
+  } else {
+    header.classList.remove('show-header')
+    header.classList.add('hide-header')
+  }
+
+  prevScrollPos = currentScrollPos
+}
